c67216901eb54663d41f21bf7d9561fa21cb9cc7
Niyomufasha <fabrice1niyo@gmail.com>
2023-04-17 06:49:23 -0700

    deleted conmponents and installed some dependencies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import Navigation from './component/Navigation'
 import About from './component/About'
 import Home from './component/Home'
-import Contact from './component/Contact'
+
 import Help from './component/Help'
 import Authenticate from './component/Authenticate'
 import { Routes, Route } from 'react-router-dom'
@@ -16,7 +16,7 @@ function App() {
       <Routes>    
          <Route exact path='/' element={<Home />} />
          <Route path='/services' element={<Services/>} />
-         <Route path='/contact' element={<Contact/>} />
+         {/* <Route path='/contact' element={<Contact/>} /> */}
          <Route path='/help' element={<Help/>} />
         <Route path='/authenticate' element={<Authenticate/>} />
         <Route path='/registration' element={<Registration/>} />
